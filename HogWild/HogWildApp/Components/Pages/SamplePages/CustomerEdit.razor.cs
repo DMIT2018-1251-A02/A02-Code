4cb9fd6b1b2b1feacd67377964b00f7ef144b7ad
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-11-06 16:27:05 -0700

    Nov 6

--- HogWild/HogWildApp/Components/Pages/SamplePages/CustomerEdit.razor.cs
+++ HogWild/HogWildApp/Components/Pages/SamplePages/CustomerEdit.razor.cs
@@ -9,31 +9,37 @@ namespace HogWildApp.Components.Pages.SamplePages
     public partial class CustomerEdit
     {
         #region Fields
-
         private string feedbackMessage = string.Empty;
         private string errorMessage = string.Empty;
         private bool hasFeedback => !string.IsNullOrWhiteSpace(feedbackMessage);
-
         private bool hasError => !string.IsNullOrWhiteSpace(errorMessage) || errorDetails.Count() > 0;
-
         //error list
         private List<string> errorDetails = new List<string>();
 
         // customer
         private CustomerEditView customer = new();
+        //  the provinces
+        private List<LookupView> provinces = new List<LookupView>();
+        //  countries
+        private List<LookupView> countries = new List<LookupView>();
+        // status lookup
+        private List<LookupView> statusLookups = new List<LookupView>();
 
         // mudform control
         private MudForm customerForm = new();
-
         #endregion
 
         #region Propertiers
-
         //  customer service
-        [Inject] protected CustomerService CustomerService { get; set; } = default!;
+        [Inject]
+        protected CustomerService CustomerService { get; set; } = default!;
+
+        [Inject]
+        protected CategoryLookupService CategoryLookupService { get; set; } = default!;
 
         //  Customer ID used to create or edit a customer
-        [Parameter] public int CustomerID { get; set; } = 0;
+        [Parameter]
+        public int CustomerID { get; set; } = 0;
 
         #endregion
 
@@ -43,16 +49,16 @@ namespace HogWildApp.Components.Pages.SamplePages
         {
             await base.OnInitializedAsync();
 
-            // clear previous error details and messages
+            //	clear previous error details and messages
             errorDetails.Clear();
             errorMessage = string.Empty;
-            feedbackMessage = String.Empty;
+            feedbackMessage = string.Empty;
 
             //  check to see if we are navigation using a valid customer CustomerID
             //      or are we going to create a new customer
             if (CustomerID > 0)
             {
-                // wrap the service call in a try/catch to handle unexpected exceptions
+                //	wrap the service call in a try/catch to handle unexpected exceptions
                 try
                 {
                     var result = CustomerService.GetCustomer(CustomerID);
@@ -67,17 +73,58 @@ namespace HogWildApp.Components.Pages.SamplePages
                 }
                 catch (Exception ex)
                 {
-                    // capture any exception message for display
+                    //	capture any exception message for display
                     errorMessage = ex.Message;
                 }
             }
             else
             {
-                customer = new CustomerEditView();
+                customer = new();
             }
-            //  update that data has changed
+
+            #region Lookups
+
+            //  province
+            var results = CategoryLookupService.GetLookups("Province");
+            if (results.IsSuccess)
+            {
+                provinces = results.Value;
+            }
+            else
+            {
+                errorDetails = HogWildHelperClass.GetErrorMessages(results.Errors.ToList());
+            }
+
+            //  countries
+            results = CategoryLookupService.GetLookups("Country");
+            if (results.IsSuccess)
+            {
+                countries = results.Value;
+            }
+            else
+            {
+                errorDetails = HogWildHelperClass.GetErrorMessages(results.Errors.ToList());
+            }
+
+            //  status lookup
+            results = CategoryLookupService.GetLookups("Customer Status");
+            if (results.IsSuccess)
+            {
+                statusLookups = results.Value;
+            }
+            else
+            {
+                errorDetails = HogWildHelperClass.GetErrorMessages(results.Errors.ToList());
+            }
+
+
+
+            #endregion
+
+            //  update that data has change
             StateHasChanged();
         }
         #endregion
+
     }
 }
---
+++ HogWild/HogWildSystem/BLL/CategoryLookupService.cs
@@ -0,0 +1,66 @@
+﻿using BYSResults;
+using HogWildSystem.DAL;
+using HogWildSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogWildSystem.BLL
+{
+    public class CategoryLookupService
+    {
+        #region Fields
+        //  hog wild context
+        private readonly HogWildContext _hogWildContext;
+        #endregion
+
+        //  constructor for the CategoryLookupService class.
+        internal CategoryLookupService(HogWildContext hogWildContext)
+        {
+            //  Initialize the _hogWildContext field with the provided HogWildContext instance.
+            _hogWildContext = hogWildContext;
+        }
+
+        public Result<List<LookupView>> GetLookups(string categoryName)
+        {
+            //  create a result container
+            var result = new Result<List<LookupView>>();
+
+            #region Business Rules
+            //  Rule:   category name cannot be empty
+            //  Rule:   RemoveFromViewFlag must be false (soft delete)
+            if (string.IsNullOrWhiteSpace(categoryName))
+            {
+                return result.AddError(new Error("Missing Information",
+                                        "Please provide a valid category name"));
+            }
+            #endregion
+
+
+            var lookups = _hogWildContext.Lookups
+                              .Where(x => x.Category.CategoryName == categoryName)
+                              .Select(x => new LookupView
+                              {
+                                  LookupID = x.LookupID,
+                                  CategoryID = x.CategoryID,
+                                  Name  = x.Name,
+                                  RemoveFromViewFlag = x.RemoveFromViewFlag
+                              })
+                              .OrderBy(x => x.Name)
+                              .ToList();
+
+            // If not lookups were found with the category name
+            if(lookups == null || lookups.Count() == 0)
+            {
+                return result.AddError(new Error("No Lookups",
+                    $"No lookups for category name '{categoryName}' was found"));
+            }
+
+            //  return the result
+            return result.WithValue(lookups);
+
+        }
+    }
+}
